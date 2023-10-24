381a0091792b959ec4352988f413165a370d7c30
esdralemes <esdralemes@gmail.com>
2023-10-24 18:22:42 -0300

    Saída de dados/Casting

---
+++ casting.py
@@ -0,0 +1,24 @@
+b1: float;
+b2: float;
+h: float;
+area: float
+b1 = 6.0
+b2 = 8.0
+h = 5.0
+area = (b1 + b2) / 2.0 * h
+print(area)
+
+a: int
+b: int
+resultado: int
+a = 5
+b = 2
+resultado = a // b
+print(resultado)
+
+a: float
+b: int
+
+a = 5.2
+b = int(a)
+print(b)
