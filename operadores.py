bb2d145ccf71e415a83455bfea6efaa2a115604c
esdralemes <esdralemes@gmail.com>
2023-10-24 17:45:36 -0300

    Operadores em Python

---
+++ operadores.py
@@ -0,0 +1,18 @@
+x = 5
+y = 2
+
+a = 5 + 2
+b = 5 - 2
+c = 5 * 3
+d = 5 / 2
+e = 10 % 2
+f = 10 ** 2
+g = 10 // 2
+
+print(f"Valor da soma {a}")
+print(f"Valor da subtração {b}")
+print(f"Valor da multiplicação {c}")
+print(f"Valor da divisão {d}")
+print(f"valor do resto da divisão {e}")
+print(f"Valor da exponenciação {f}")
+print(f"Valor da divisão inteira {g}")
